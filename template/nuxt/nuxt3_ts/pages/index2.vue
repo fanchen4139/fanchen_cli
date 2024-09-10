7535ee93892562ba2c596c86e964bec096539b9b
fanjindai <fanchen4139@163.com>
2024-09-10 15:46:38 +0800

    feat(nuxt-app): 增加渲染逻辑

--- template/nuxt/nuxt3_ts/pages/index2.vue
+++ template/nuxt/nuxt3_ts/pages/index2.vue
@@ -1,8 +1,28 @@
 <template>
-  <div>index2</div>
+  <div>
+    <ul>
+      <li v-for="item in 50000">{{ item }}</li>
+    </ul>
+  </div>
 </template>
 
-<script setup lang="ts">
+<script setup>
+let timing = window.performance.timing
+let start = timing.navigationStart
+let dnsTime = 0
+let tcpTime = 0
+let firstPaintTime = 0
+let domRenderTime = 0
+let loadTime = 0
+
+dnsTime = timing.domainLookupEnd - timing.domainLookupStart;
+tcpTime = timing.connectEnd - timing.connectStart;
+firstPaintTime = timing.responseStart - start;
+domRenderTime = timing.domContentLoadedEventEnd - start;
+loadTime = timing.loadEventEnd - start;
+
+console.log('DNS解析时间:', dnsTime, '\nTCP建立时间:', tcpTime, '\n首屏时间:', firstPaintTime,
+  '\ndom渲染完成时间:', domRenderTime, '\n页面onload时间:', loadTime);
 </script>
 
 <style lang="less" scoped>
